5a171857245128c0d495b0f21cfeb67ac2016144
Tamas Juhasz <tamasjuhasz84@yahoo.com>
2023-07-24 22:34:23 +0200

    fix:connection

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ const sequelize = new Sequelize({
     storage: "./database.sqlite"
   });
 
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require( 'express' );
+const app     = express();
+const port    = process.env.PORT || 1024;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  });
+app.listen( port, console.log( `http://127.0.0.1:${ port }` ) );
 
 (async () => {
     const users = await sequelize.query("SELECT * FROM `Employees`", { type: QueryTypes.SELECT });
-    app.get('/', (req, res) => {        
+    app.get('/', (req, res) => {     
             res.send('A teljes adatbázis jelenleg:<br><br>'+JSON.stringify(users))
-    })
+    });
 })();
