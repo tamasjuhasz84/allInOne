d968ec14a5c52c4a46e94246b75c859c8335ff6f
Tamas Juhasz <tamasjuhasz84@yahoo.com>
2023-07-24 22:58:10 +0200

    fix:connection

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ app.listen( port, console.log( `http://127.0.0.1:${ port }` ) );
 
 (async () => {
     const users = await sequelize.query("SELECT * FROM `Employees`", { type: QueryTypes.SELECT });
-    app.get('/', (req, res) => {     
-            res.send('A teljes adatbázis jelenleg:<br><br>'+JSON.stringify(users))
+    app.get('/', (req, res) => {
+      res.type('application/json');    
+            res.send(users);
     });
 })();
