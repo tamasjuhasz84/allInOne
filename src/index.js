17326e53046a2e551ba443fcc4a91fef6f32c28d
Tamas Juhasz <tamasjuhasz84@yahoo.com>
2023-07-25 20:13:42 +0200

    chore: practice.html + public elements

--- src/index.js
+++ src/index.js
@@ -4,16 +4,23 @@ const sequelize = new Sequelize({
     storage: "./database.sqlite"
   });
 
+const path = require('path');
 const express = require( 'express' );
 const app     = express();
 const port    = process.env.PORT || 1024;
 
-app.listen( port, console.log( `http://127.0.0.1:${ port }` ) );
+app.use(express.static('public'));
+
+app.all('/',(req, res)=>{
+    res.sendFile(path.join(__dirname + '/practice.html'));
+});
 
 (async () => {
-    const users = await sequelize.query("SELECT * FROM `Employees`", { type: QueryTypes.SELECT });
-    app.get('/', (req, res) => {
+  const users = await sequelize.query("SELECT * FROM `Employees`", { type: QueryTypes.SELECT });
+  app.get('/users', (req, res) => {
       res.type('application/json');    
-            res.send(users);
-    });
+      res.send(users);
+  });
 })();
+
+app.listen( port, console.log( `http://127.0.0.1:${ port }` ) );
