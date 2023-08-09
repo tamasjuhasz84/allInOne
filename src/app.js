2c0a24c26df1c952228aae83db609c6da5aa688b
JuhaszTamas1984 <tamas.juhasz@idomsoft.hu>
2023-08-09 16:58:42 +0200

    feat(hooks): some server hook

--- src/app.js
+++ src/app.js
@@ -1,34 +1,47 @@
 //Database connection part
 const { Sequelize, QueryTypes, DataTypes } = require("sequelize");
 const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./database.sqlite"
-  });
+  dialect: "sqlite",
+  storage: "./database.sqlite"
+});
 
-  const User = sequelize.define("user", {
-    name: DataTypes.TEXT,
-    age: DataTypes.INTEGER,
-    os: {
-      type: DataTypes.TEXT,
-      defaultValue: 'windows'
+const User = sequelize.define("user", {
+  name: DataTypes.TEXT,
+  age: DataTypes.INTEGER,
+  os: {
+    type: DataTypes.TEXT,
+    defaultValue: 'windows'
+  },
+  activeWorker: DataTypes.BOOLEAN,
+  pw: DataTypes.INTEGER,
+}, {
+  tableName: 'Employees',
+  hooks: {
+    beforeCreate: (user, options) => {
+      if (user.name == 'Admin') {
+        throw new Error('Admin user cannot be created');
+      }
     },
-    activeWorker: DataTypes.BOOLEAN,
-    pw: DataTypes.INTEGER,
-  }, {
-    tableName: 'Employees'
-  });
+    afterCreate: (user, options) => {
+      console.log('Sikeres adatbázis művelet!');
+      console.log(user.name);
+      console.log(user.age);
+    }
+  }
+});
 
 
 const path = require('path');
 const bodyParser = require('body-parser');
-const express = require( 'express' );
+const express = require('express');
 const app = express();
+var admin = express()
 //const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 1024;
 const routerForUsers = require('./routerForUsers');
 
-app.use('/users',routerForUsers);
+app.use('/users', routerForUsers);
 //app.use(helmet());
 app.use(cookieParser());
 app.use(express.static('public'));
@@ -36,19 +49,19 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 //log config part
 const log4js = require("log4js");
 const logger = log4js.getLogger();
 log4js.configure({
-  appenders: { 
+  appenders: {
     app: { type: "file", filename: "app.log" }
-   },
-  categories: { 
-    default: { 
+  },
+  categories: {
+    default: {
       appenders: ['app'],
-      level: "debug" 
+      level: "debug"
     }
   }
 });
@@ -67,11 +80,11 @@ function subtraction(a, b) {
 };
 
 function speed(a, b) {
-  return (a / b)/3.6;
+  return (a / b) / 3.6;
 };
 
-async function employees (req,res,next){
-  res.locals.employees = await sequelize.query("SELECT * FROM `Employees`", { type: QueryTypes.SELECT }); 
+async function employees(req, res, next) {
+  res.locals.employees = await sequelize.query("SELECT * FROM `Employees`", { type: QueryTypes.SELECT });
   next();
 };
 
@@ -81,34 +94,35 @@ module.exports.division = division;
 module.exports.subtraction = subtraction;
 
 app.use(employees);
-app.use((req, res, next)=>{
-  if(req.query.errorMes === 'unauthozized'){
-      res.locals.errorMes = `Validation problem.`
-  }else{
-      res.locals.errorMes = ``
+app.use((req, res, next) => {
+  if (req.query.errorMes === 'unauthozized') {
+    res.locals.errorMes = `Validation problem.`
+  } else {
+    res.locals.errorMes = ``
   }
   next()
 });
 
-app.get('/', (req, res, next)=>{
+app.get('/', (req, res, next) => {
   const errorMes = req.query.errorMes;
-  if(errorMes === 'unauthozized'){
+  if (errorMes === 'unauthozized') {
   }
   res.render('login')
 });
 
-app.post('/process_login',(req, res, next)=>{
+app.post('/process_login', (req, res, next) => {
   const password = req.body.password;
   const username = req.body.username;
   (async () => {
-    res.locals.pass = await sequelize.query("SELECT pw FROM `Employees` WHERE name LIKE :search_name", { 
-      replacements: { search_name: username }, type: QueryTypes.SELECT }); 
-  
-    if(password == res.locals.pass[0].pw){
-      res.cookie('username',username)
+    res.locals.pass = await sequelize.query("SELECT pw FROM `Employees` WHERE name LIKE :search_name", {
+      replacements: { search_name: username }, type: QueryTypes.SELECT
+    });
+
+    if (password == res.locals.pass[0].pw) {
+      res.cookie('username', username)
       res.redirect('/welcome')
       logger.info(username + ' logged in!')
-    }else{
+    } else {
       res.redirect('/?msg=unauthorized')
       logger.warn('Unauthozized attempt!')
     }
@@ -116,58 +130,59 @@ app.post('/process_login',(req, res, next)=>{
   })();
 });
 
-app.get('/welcome',(req, res, next)=>{
-  if(typeof req.cookies.username === 'undefined') {
+app.get('/welcome', (req, res, next) => {
+  if (typeof req.cookies.username === 'undefined') {
     res.redirect('/?msg=unauthorized')
-  }else{
-    res.render('welcome',{
+  } else {
+    res.render('welcome', {
       username: req.cookies.username,
       firstKey: "Ez az első kulcs értéke!",
       secondKey: "Ez a második kulcs értéke!"
     });
-}});
+  }
+});
 
-app.param('id',(req, res, next, id)=>{
+app.param('id', (req, res, next, id) => {
   next();
 });
 
-app.get('/info/:infoId/:link',(req, res, next)=>{
+app.get('/info/:infoId/:link', (req, res, next) => {
   res.send(`<h1>Main ${req.params.infoId} - ${req.params.link}</h1><p><a href="/logout">Log out</a></p>`)
 });
 
-app.get('/functions',(req, res, next)=>{
-  if(typeof req.cookies.username === 'undefined') {
-      res.redirect('/?msg=unauthorized')
-  }else{
-      res.render('functions', {
-        resultNum: req.cookies.resultNum,
-        speedResultNum: req.cookies.speedResultNum,
-        resDataMes: req.cookies.resDataMes,
-        countEmployees: req.cookies.countEmployees
-      });
+app.get('/functions', (req, res, next) => {
+  if (typeof req.cookies.username === 'undefined') {
+    res.redirect('/?msg=unauthorized')
+  } else {
+    res.render('functions', {
+      resultNum: req.cookies.resultNum,
+      speedResultNum: req.cookies.speedResultNum,
+      resDataMes: req.cookies.resDataMes,
+      countEmployees: req.cookies.countEmployees
+    });
   }
   next();
 });
 
-app.post('/process_multiplication', (req, res, next)=>{
+app.post('/process_multiplication', (req, res, next) => {
   res.clearCookie('resultNum');
   const resultNum = multiplication(req.body.firstNum, req.body.secondNum);
-  res.cookie('resultNum',resultNum);
+  res.cookie('resultNum', resultNum);
   logger.debug('Szorzás funkció eredménye: ' + resultNum);
   res.redirect('/functions');
   next();
 });
 
-app.post('/process_speed', (req, res, next)=>{
+app.post('/process_speed', (req, res, next) => {
   res.clearCookie('speedResultNum');
   const speedResultNum = speed(req.body.distance, req.body.time);
-  res.cookie('speedResultNum',speedResultNum);
+  res.cookie('speedResultNum', speedResultNum);
   logger.debug('Átváltás eredménye : ' + speedResultNum);
   res.redirect('/functions');
   next();
 });
 
-app.post('/process_create', (req, res, next)=>{
+app.post('/process_create', (req, res, next) => {
   res.clearCookie('resDataMes');
   const name = req.body.name;
   const age = req.body.age;
@@ -179,40 +194,40 @@ app.post('/process_create', (req, res, next)=>{
       await sequelize.sync();
       await User.create({ name: name, age: age, os: os, activeWorker: activeWorker, pw: pw });
     })();
-    res.cookie('resDataMes',"Sikeres beküldés!");
+    res.cookie('resDataMes', "Sikeres beküldés!");
     logger.debug('Sikeres beküldés!');
     res.redirect('/functions');
-  }else{
-    res.cookie('resDataMes',"Sikertelen beküldés!");
+  } else {
+    res.cookie('resDataMes', "Sikertelen beküldés!");
     logger.debug('Sikertelen beküldés!');
     res.redirect('/functions');
   }
   next();
 });
 
-app.post('/process_query', (req, res, next)=>{
+app.post('/process_query', (req, res, next) => {
   res.clearCookie('countEmployees');
   var countEmployees = res.locals.employees.length;
-  res.cookie('countEmployees',countEmployees);
+  res.cookie('countEmployees', countEmployees);
   logger.debug('A dolgozók száma összesen : ' + countEmployees);
   res.redirect('/functions');
   next();
 });
 
-app.get('/events',(req, res, next)=>{
-  if(typeof req.cookies.username === 'undefined') {
-      res.redirect('/?msg=unauthorized')
-  }else{
-      res.render('events', {
-        firstEvent: multiplication(5,5),
-        secondEvent: division(10,2),
-        thirdEvent: subtraction(20,7)
-      });
-  };  
+app.get('/events', (req, res, next) => {
+  if (typeof req.cookies.username === 'undefined') {
+    res.redirect('/?msg=unauthorized')
+  } else {
+    res.render('events', {
+      firstEvent: multiplication(5, 5),
+      secondEvent: division(10, 2),
+      thirdEvent: subtraction(20, 7)
+    });
+  };
   next();
 });
 
-app.get('/logout',(req, res, next)=>{
+app.get('/logout', (req, res, next) => {
   res.clearCookie('username');
   res.clearCookie('resultNum');
   res.clearCookie('speedResultNum');
@@ -221,5 +236,4 @@ app.get('/logout',(req, res, next)=>{
   res.redirect('/')
 });
 
-
-app.listen( port, console.log( `http://127.0.0.1:${ port }` ) );
+app.listen(port, console.log(`http://127.0.0.1:${port}`));
--- src/server.js
+++ src/server.js
@@ -14,15 +14,26 @@ const User = sequelize.define("user", {
   activeWorker: DataTypes.BOOLEAN,
   pw: DataTypes.INTEGER,
 }, {
-  tableName: 'Employees'
+  tableName: 'Employees',
+  hooks: {
+    beforeCreate: (user, options) => {
+      if (user.name == 'Admin') {
+        throw new Error('Admin user cannot be created');
+      }
+    },
+    afterCreate: (user, options) => {
+      console.log('Sikeres adatbázis művelet!');
+      console.log(user.name);
+      console.log(user.age);
+    }
+  }
 });
 
 (async () => {
-    await sequelize.drop();
-    await sequelize.sync({ force: true });
-    await User.create({ name: "Tamas", age: 39, activeWorker: true, pw: 123 });
-    await User.create({ name: "Balazs", age: 25, os: "Linux", activeWorker: true, pw: 456 });
-    await User.create({ name: "Anna", age: 45, activeWorker: true, pw: 789 });
-    await User.create({ name: "Imre", age: 30, os: "Linux", activeWorker: false, pw: 987 });
+  await sequelize.drop();
+  await sequelize.sync({ force: true });
+  await User.create({ name: "Tamas", age: 39, activeWorker: true, pw: 123 });
+  await User.create({ name: "Balazs", age: 25, os: "Linux", activeWorker: true, pw: 456 });
+  await User.create({ name: "Anna", age: 45, activeWorker: true, pw: 789 });
+  await User.create({ name: "Imre", age: 30, os: "Linux", activeWorker: false, pw: 987 });
 })();
-
